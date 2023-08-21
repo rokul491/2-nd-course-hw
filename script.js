da5b40ef64b9d9a954c0526ab7fcd71ad7fbe9c8
Roman Kulik <kurok444@gmail.com>
2023-08-21 20:27:41 +0300

    hw-3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //1
-let a = 10;
+/*let a = 10;
 alert(a);
 a = 20;
 alert(a);
@@ -56,8 +56,90 @@ delete user["city of residence"];
 
 
 let info = prompt(`Какую информацию хотите узнать о пользователе?`);
-alert(user.info);
+alert(user[info]);
 
 //10
 let userName = prompt(`Как тебя зовут?`);
 alert(`Привет, ${userName}!`); 
+
+//
+// Lesson 3
+//
+
+//1
+let password = 'пароль';
+let checkPassword = prompt('Введите пароль');
+
+if (checkPassword === password) {
+    alert('Пароль введен верно');
+} else {
+    alert('Пароль введен неправильно');
+}
+
+//2
+let c = 2;
+if (c > 0 && c < 10) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+//3
+let d = 93;
+let e = 105;
+ if (d > 100 || e > 100) {
+    console.log('Верно');
+ } else {
+    console.log('Неверно');
+ }
+
+ //4
+ let a = '2';
+ let b = '3';
+
+ a = Number(a);
+ b = Number(b);
+ alert(a + b);
+
+ //5
+let monthNumber = 13;
+switch(monthNumber) {
+    case 1:
+        alert('Зима');
+        break;
+    case 2:
+        alert('Зима');
+        break;
+    case 3:
+        alert('Весна');
+        break;
+    case 4:
+        alert('Весна');
+        break;
+    case 5:
+        alert('Весна');
+        break;
+    case 6:
+        alert('Лето');
+        break;
+    case 7:
+        alert('Лето');
+        break;
+    case 8:
+        alert('Лето');
+        break;
+    case 9:
+        alert('Осень');
+        break;
+    case 10:
+        alert('Осень');
+        break;
+    case 11:
+        alert('Осень');
+        break;
+    case 12:
+        alert('Зима');
+        break;
+    default:
+        alert('Неверный номер месяца');    
+}*/
