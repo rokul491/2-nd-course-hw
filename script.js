62b2106645ff7e247c63aa538245e295ead2da62
Roman Kulik <kurok444@gmail.com>
2023-08-21 20:28:11 +0300

    hw-3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //1
-/*let a = 10;
+let a = 10;
 alert(a);
 a = 20;
 alert(a);
@@ -142,4 +142,4 @@ switch(monthNumber) {
         break;
     default:
         alert('Неверный номер месяца');    
-}*/
+}
